e787d60ac02fb1c5e0f5fcc188674f238e85cae9
chikchicly <chikchicly@gamil.com>
2022-04-10 13:25:42 +0700

    json

---
+++ positive-thinking/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Article from './article';
+import './index.css';
+
+
+
+ReactDOM.render(
+  <Article/>,
+  document.querySelector('article')
+);
